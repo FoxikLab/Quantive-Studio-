ecf4c1903b6355b821e481986ae58307c92c25cb
FoxikLab <ourednicektom@gmail.com>
2025-09-28 18:46:48 +0200

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Crew interakce přijdou později
 console.log("Quantive Studio™ loaded");
+
+document.getElementById("enterBtn").addEventListener("click", function() {
+  document.getElementById("crewSection").style.display = "block";
+  this.style.display = "none"; // schová tlačítko po kliknutí
+});
